a7cd3c348b8a212e888c7cccfd16c51e73b48c5c
YasirAl-tuwaijri <yasir.altuwaijri@gmail.com>
2025-07-19 04:16:52 +0300

    Add files via upload

---
+++ The-challenging-Fallout-New-Vegas-speedrun.py
@@ -0,0 +1,15 @@
+import random
+
+choices1 = ["Yes man ", "legion", "House", " NCR",]
+draw1 = random.choice(choices1)
+print("First random:", draw1)
+
+# القرعة المرافق (7 خيارات)
+choices2 = [" Arcade", " Ed-E", " Rex", " Cass", " Boone", " lily", "Raul"]
+draw2 = random.choice(choices2)
+print("Second random:", draw2)
+
+# القرعة التحدي  (4 خيارات)
+choices3 = [" No gambling", " Killing a deathKlaw in the same save", " doing enclave side quest", " doing BoS side quest"]
+draw3 = random.choice(choices3)
+print("third random:", draw3)
