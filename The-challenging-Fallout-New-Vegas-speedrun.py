c8d2c7f89f3608858b7a744062ccb737b647d4ae
YasirAl-tuwaijri <yasir.altuwaijri@gmail.com>
2025-07-19 22:03:22 +0300

    The-challenging-Fallout-New-Vegas-speedrun.py

--- The-challenging-Fallout-New-Vegas-speedrun.py
+++ The-challenging-Fallout-New-Vegas-speedrun.py
@@ -4,12 +4,11 @@ choices1 = ["Yes man ", "legion", "House", " NCR",]
 draw1 = random.choice(choices1)
 print("First random:", draw1)
 
-# القرعة المرافق (7 خيارات)
-choices2 = [" Arcade", " Ed-E", " Rex", " Cass", " Boone", " lily", "Raul"]
+# Randomizng the companion choice (they should be with you until the ending unless they lefft you beacuse your ending choices)
+choices2 = [" Arcade", " Ed-E", " Rex", " Cass", " Boone", " Lily", " Raul", " Veronica"]
 draw2 = random.choice(choices2)
 print("Second random:", draw2)
-
-# القرعة التحدي  (4 خيارات)
-choices3 = [" No gambling", " Killing a deathKlaw in the same save", " doing enclave side quest", " doing BoS side quest"]
+# Randomizng the challenge while speed running
+choices3 = [" No gambling", " Killing a deathclaw without loading a save", " doing enclave side quest", " doing BoS side quest"]
 draw3 = random.choice(choices3)
 print("third random:", draw3)
